a491abc3e4030596261de0905f777f00c2aec024
Ibrahim Zade <sergey.vladimirovich@gmail.com>
2021-05-22 14:24:28 +0300

    Add segments A, B and C

--- polys.py
+++ polys.py
@@ -1,18 +1,39 @@
-import sys  # For argv
+import sys, math  # For argv
 from PySide6 import QtCore, QtGui, QtWidgets
 from PySide6.QtWidgets import *
-from PySide6.QtGui import QGradient, QLinearGradient, QPen, QBrush, QColor, QPainter
-from PySide6.QtCore import QPoint, QRect, QRectF, QTimer, Qt
+from PySide6.QtGui import QGradient, QLinearGradient, QPen, QBrush, QColor, QPainter, QPolygonF
+from PySide6.QtCore import QPoint, QRect, QRectF, QPointF, QTimer, Qt
 
 class MainApp(QtWidgets.QMainWindow):
     resized = QtCore.Signal()
+
+    segAPoints = [
+        QPointF(0.0, 0.6), QPointF(0.1, 0.25), QPointF(0.25, 0.0),
+        QPointF(1.0, 0.0), QPointF(0.9, 1.0), QPointF(0.4, 1.0),
+        QPointF(0.0, 0.6)
+    ]
+
+    segBPoints = [
+        QPointF(0.5, 0.0), QPointF(0.75, 0.1), QPointF(1.0, 0.25),
+        QPointF(0.9, 0.8), QPointF(0.65, 1.0), QPointF(0.0, 0.8),
+        QPointF(0.12, 0.25)
+    ]
+
+    segCPoints = [
+        QPointF(0.75, 0.0), QPointF(1.0, 0.2), QPointF(0.9, 0.75),
+        QPointF(0.75, 0.9), QPointF(0.5, 1.0), QPointF(0.0, 0.75),
+        QPointF(0.12, 0.2)
+    ]
+
     def __init__(self):
         super().__init__()
-        
-        self.pen = QPen(QColor(0,0,0))                      # set lineColor
-        self.pen.setWidth(3)                                            # set lineWidth
-        self.brush = QBrush(QColor(255,255,255,255))        # set fillColor  
-        self.polygon = self.createPoly(8,150,0)                         # polygon with n points, radius, angle of the first point
+        self.polygons = []
+        self.pen = QPen(Qt.green)                      # set lineColor
+        self.pen.setWidth(1)                                            # set lineWidth
+        self.brush = QBrush(Qt.green)        # set fillColor  
+        self.polygons.append(self.createPoly(QRectF(10, 10, 200, 50), self.segAPoints)) # polygon with n points, radius, angle of the first point
+        self.polygons.append(self.createPoly(QRectF(200, 10, 60, 200), self.segBPoints)) # polygon with n points, radius, angle of the first point
+        self.polygons.append(self.createPoly(QRectF(190, 220, 60, 200), self.segCPoints)) # polygon with n points, radius, angle of the first point
 
     def resizeEvent(self, event):
         self.resized.emit()
@@ -25,8 +46,16 @@ class MainApp(QtWidgets.QMainWindow):
     def paintEvent(self, event):
         painter = QtGui.QPainter(self)
         painter.setPen(self.pen)
-        painter.setBrush(self.brush)  
-        painter.drawPolygon(self.polygon)
+        painter.setBrush(self.brush)
+        for polygon in self.polygons:
+            painter.drawPolygon(polygon)
+
+    def createPoly(self, boundingBox, polyPoints):
+        polygon = QtGui.QPolygonF()
+        for point in polyPoints: 
+            polygon.append(QPointF(point.x() * boundingBox.width(), point.y() * boundingBox.height()))  
+        polygon.translate(boundingBox.topLeft())
+        return polygon
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
